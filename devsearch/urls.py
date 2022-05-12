b7db2db03a388767594a9d70e9ede77d2130fe0f
Enache Andrei <enache.andrei.george@gmail.com>
2022-05-13 00:34:55 +0300

    added urls

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -14,14 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
+from django.urls import path, include
 
-
-def projects(request):
-    return HttpResponse('Here are our products!')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('projects/', projects, name="projects"),
+    path('', include('projects.urls'))
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+
+def projects(request):
+    return HttpResponse('Here are our products!')
+
+def project(request, pk):
+    return HttpResponse('Single PROJECT '+str(pk))
