ffafff8d437ae23aefed978d35bca6d2c22bd907
Enache Andrei <enache.andrei.george@gmail.com>
2022-05-13 20:15:00 +0300

    Added dynamic modification of the template

--- projects/views.py
+++ projects/views.py
@@ -3,9 +3,33 @@ from django.http import HttpResponse
 # Create your views here.
 
 
+projectList = [
+    {
+        'id':'1',
+        'title':'Ecommerce Website',
+        'description': 'Fully functional ecommerce website'
+    },
+        {
+        'id':'2',
+        'title':'Portofolio Website',
+        'description': "This is a project that i've built for my portofolio!"
+    },
+        {
+        'id':'3',
+        'title':'Social Network',
+        'description': 'Awesome open source priject I am still working at'
+    },
+]
 
 def projects(request):
-    return HttpResponse('Here are our products!')
+    page = 'Projects'
+    number = 10
+    context =  {'page': page, 'number': number, 'projects': projectList}
+    return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
-    return HttpResponse('Single PROJECT '+str(pk))
+    projectObj = None
+    for proj in projectList:
+        if proj['id'] == pk:
+            projectObj = proj
+    return render(request, 'projects/single-project.html', {'project': projectObj})
