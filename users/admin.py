d3229b6becdfd2349484cadbae50ff5a38d2510d
Enache Andrei <enache.andrei.george@gmail.com>
2022-05-17 00:36:03 +0300

    Added profile and profiles pages

--- users/admin.py
+++ users/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Profile
+from .models import Profile, Skill
 
+# It shows up in the Admin pannel just after we register it here
 admin.site.register(Profile)
+admin.site.register(Skill)
